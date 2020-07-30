b320122c36e7e66dcd566b50f5617575b03927e6
Alasdair Clarke <a.clarke@essex.ac.uk>
2020-07-30 12:53:50 +0100

    import Experients 1a, 1b, 1c, 2a, 2b.

--- reanalyse_Buetti2019/ac_analysis.R
+++ reanalyse_Buetti2019/ac_analysis.R
@@ -1,48 +1,57 @@
 library(tidyverse)
 library(readxl)
 
-# Experiment 1
+import_experiment <- function(sheet, d_labels) {
 
-read_excel(
+	read_excel(
 	"../previous_work/Buetti2019_data_code/OSF_originaldata.xlsx", 
-	sheet = 2) %>%
+	sheet = sheet) %>%
 	# use tidier variable names
 	select(
 		p_id = "Subject",
 		trial = "Trial",
 		t_id = "tid",
 		n = "numd",
-		colour = "dcolors",
+		d = "dcolors",
 		rt = "RT",
 		response = "resp",
 		error = "Error") %>%
 	# code up p_id, t_id and distracter colour as a factor
 	mutate(
 		p_id = as_factor(p_id),
-		colour = as_factor(colour),
-		colour = fct_recode(colour, 
-			orange = "1", yellow = "2", blue = "3", none = "0"),
+		d = as_factor(d),
+		d = fct_recode(d, !!!d_labels),
 		t_id = as_factor(t_id),
 		t_id = fct_recode(t_id, left = "0", right = "1")) %>%
 	# remove error trials
 	filter(error == 0) -> d
 
+	return(d)
+}
+
+d_1a <- import_experiment(2,  c(orange = "1", yellow = "2", blue = "3"))
+d_1b <- import_experiment(4,  c(diamond = "1", circle = "2", triangle = "3"))
+d_2a <- import_experiment(6,  c(`orange diamond` = "1", `blue circle` = "2", `yellow triangle` = "3"))
+d_2b <- import_experiment(8,  c(`orange circle` = "1", `yellow diamond` = "2", `blue triangle` = "3"))
+d_2c <- import_experiment(10, c(`blue diamond` = "1", `yellow circle` = "2", `orange triangle` = "3"))
+
+
 #do some simple counts... does these numbers match those in the paper?
 
-d %>% group_by(p_id, colour) %>%
+d_1a %>% group_by(p_id, colour) %>%
 summarise(trials = n(),
 	mean_rt = mean(rt))
 
 # facet plot of all the correct RTs
 
-d %>% ggplot(aes(x = n, y = log(rt), colour = colour)) + 
+d_1a %>% ggplot(aes(x = n, y = log(rt), colour = colour)) + 
 	geom_jitter(alpha = 0.25) + 
 	geom_smooth(method = "lm", se = F) + 
 	facet_wrap(~ p_id) + 
 	scale_colour_manual(values = c("grey50", "darkorange3", "yellow3", "dodgerblue2"))	
 
 # show individual differences in search slopes 
-d %>% ggplot(aes(x = n, y = log(rt), colour = colour, group = p_id)) + 
+d_1a %>% ggplot(aes(x = n, y = log(rt), colour = colour, group = p_id)) + 
 	geom_smooth(method = "lm", se = T) +
 	facet_wrap(~ colour) + 
 	scale_colour_manual(values = c("darkorange3", "yellow3", "dodgerblue2"))	
