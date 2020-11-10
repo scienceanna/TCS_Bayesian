cd90a2423462af43620d3011008de2b3bd8f3299
Alasdair <alasdair.clarke@gmail.com>
2020-11-10 13:18:15 +0000

    new models

--- analysis/scripts/our_functions.R
+++ analysis/scripts/our_functions.R
@@ -3,7 +3,13 @@ set_up_model <- function(experiment, df, fam = "lognormal") {
   # this function get's everything ready for running our model
   # mainly, this involves defining priors.
   
-  # subset data to take just th experiment that we're instered in
+  # check fam input is ok!
+  if (!(fam %in% c("normal", "lognormal", "shifted_lognormal"))) {
+    print("error")
+    stop()
+  }
+  
+  # subset data to take just th experiment that we're inserted in
   df %>%
     filter(exp_id == experiment) %>%
     group_by(exp_id, p_id, d_feature, N_T) %>%
@@ -31,7 +37,7 @@ set_up_model <- function(experiment, df, fam = "lognormal") {
       prior_string("normal(-0.5, 0.1)", class = "b", coef = intercepts),
       prior_string("normal(0, 1)", class = "b", coef = slopes))
   
-  } else if(fam == "shifted") {
+  } else if(fam == "shifted_lognormal") {
     
     my_prior <- c(
       prior_string("normal(-0.5, 0.1)", class = "b", coef = intercepts),
@@ -62,8 +68,8 @@ run_model <- function(my_inputs, ppc) {
   # ppc = 'TRUE'only to carry out a prior predictive check
   
   # set number of chains and iterations per chain:
-  n_chains = 1
-  n_itr = 1000
+  n_chains = 2
+  n_itr = 2000
   
   # if we are only sampling from the prior, we will use a small subset of the data
   # this is done to optimistically make things go faster!
@@ -73,8 +79,7 @@ run_model <- function(my_inputs, ppc) {
       summarise(rt = sample(rt, 1), .groups = "drop") -> my_inputs$df
   }
   
-  # now run model, depending on choice of distribution
-
+  # now run model
   m <- brm(
     my_inputs$my_f, data = my_inputs$df,
     family = brmsfamily(my_inputs$my_dist),
@@ -89,23 +94,31 @@ run_model <- function(my_inputs, ppc) {
   
 }
   
-plot_model_fits_ex <- function(df, experiment, m, people2plot, inc_re = NA) {
+plot_model_fits_ex <- function(df, e_id, m, inc_re = NA) {
   
+  # plot search slopes for experiment e_id
+  
+  # take the experiment we want, and remove the N_T == 0 case.
   df %>%
     filter(
-      exp_id == experiment, N_T > 0) %>%
+      exp_id == e_id, N_T > 0) %>%
     mutate(
       d_feature = fct_drop(d_feature),
       p_id = fct_drop(p_id)) -> d_plt
   
+  # for plotting, sample 500 data points at random
+  # set seed so each of the three model plots has the same  example points
+  set.seed(1011) 
   d_plt <- sample_n(d_plt, 500)
   
+  # plot 53% and 97% intervals for the model, and overall our randomly sampled
+  # data points
   d_plt %>%
     modelr::data_grid(p_id, N_T= seq(0,36,4), d_feature) %>%
     add_predicted_draws(m, re_formula = inc_re) %>% 
     ggplot(aes(x = log(N_T+1), y = .prediction)) + 
-    stat_lineribbon(.width = c(0.59, 0.73, 0.89)) + 
-    geom_point(
+    stat_lineribbon(.width = c(0.53, 0.97)) + 
+    geom_jitter(
       data = d_plt, 
       aes(y = rt), 
       alpha = 0.25, colour = "darkred") + 
@@ -118,17 +131,22 @@ plot_model_fits_ex <- function(df, experiment, m, people2plot, inc_re = NA) {
   return(plt)
 }
 
-extract_fixed_slopes_from_model <- function(m, df) {
+extract_fixed_slopes_from_model <- function(m) {
   
+  # Extract the slopes from the rt models.
+  # These values are called 'D' in the original paper
 
+  # get the slopes from the model
   slopes <- str_subset(get_variables(m), "b_d_[a-z]*:")
-  
+
+  # get samples from the posterior for these slopes
   samples <- posterior_samples(m, slopes, add_chain = TRUE) %>%
     pivot_longer(starts_with("b_d"), names_to = "d_feature", values_to = "D") %>%
     mutate(
       d_feature = as_factor(d_feature)) %>%
     select(d_feature, D, iter)
   
+  # tidy up factor labels
   levels(samples$d_feature) <- str_extract(
     levels(samples$d_feature), "(?<=feature)[a-z]+(?=:logN_TP1)")
   
@@ -138,12 +156,26 @@ extract_fixed_slopes_from_model <- function(m, df) {
 
 calc_D_overall_b <- function(f, Dx, De)
 {
+  
+  # calculate the D_overall value. 
+  # Dx are the slopes (D values) from experiment 1 (or 3)
+  # De are the slopes from experiment 2 (or 4) [e stands for empirical]
+  
+  # remember, that Dx, De and our output Dps are all big long vectors of samples
+  # from the model's posterior
+  
+  # f is the compound feature that we wish to calculate D_overall for
+  # i.e. f = "blue triangle"
+  
+  # first, get the two individual features...
   f1 <- word(f, 1)
   f2 <- word(f, 2)
   
+  # ... and their related Dx values
   D1 = as.numeric(filter(Dx, d_feature == f1)$D)
   D2 = as.numeric(filter(Dx, d_feature == f2)$D)
   
+  # now calculate D_overall using the three proposed methods
   D_collinear = 1/((1/D1) + (1/D2))
   D_best_feature = pmin(D1, D2)
   D_orth_contrast =  sqrt(1/((1/D1^2) + (1/D2^2)))
@@ -190,6 +222,7 @@ get_Dp_samples <- function(e_id, d, Dx, De) {
 set_up_predict_model <- function(e_id, df, fam = "lognormal",  meth, Dp_summary) {
   
   # this function get's everything ready for running our model
+  # this is the prediction version of the set_up_model() function at the top of the script
   
   df %>%
     filter(exp_id == e_id) %>%
@@ -215,8 +248,7 @@ set_up_predict_model <- function(e_id, df, fam = "lognormal",  meth, Dp_summary)
     filter(d_feature != "no distractors") %>%
     mutate(
       d_feature = fct_drop(d_feature),
-      d_feature = as.factor(as.character(d_feature)),
-      d_feature =  gsub("[[:space:]]", "", d_feature)) -> df
+      d_feature = as.factor(as.character(d_feature))) -> df
   
   
   if(e_id == 2) {
@@ -227,12 +259,15 @@ set_up_predict_model <- function(e_id, df, fam = "lognormal",  meth, Dp_summary)
   intercepts <- paste("d_feature", unique(df$d_feature), sep = "")
   intercepts <- gsub("[[:space:]]", "", intercepts)
   
+  slopes <- paste("d_feature", unique(df$d_feature), ":logN_TP1", sep = "")
+  slopes <- gsub("[[:space:]]", "", slopes)
+  
   
   model_sum <- round(summary(m)$fixed, 3)
   
   my_prior <-  c(
     prior_string(paste("normal(", model_sum[1:length(intercepts),1], ",",  model_sum[1:length(intercepts),2], ")", sep = ""), class = "b", coef = intercepts),
-    prior_string(paste("normal(", Dp_summary$mu, ",",  Dp_summary$sigma, ")", sep = ""), class = "b", coef = Dp_summary$d_feature),
+    prior_string(paste("normal(", Dp_summary$mu, ",",  Dp_summary$sigma, ")", sep = ""), class = "b", coef = slopes),
 prior(normal(0.25, 0.01), class = "sigma"))
   
   
@@ -253,8 +288,6 @@ predict_rt_b <- function(e_id, m, df) {
     mutate(d_feature = fct_drop(d_feature))
   
   
-    # mutate(d_feature = gsub("[[:space:]]", "", d_feature))
-
   d_out <- df_test %>%
     modelr::data_grid(d_feature = unique(d_feature), N_T = unique(N_T)) %>%
     add_fitted_draws(m, scale = "response", re_formula = NA) %>%
