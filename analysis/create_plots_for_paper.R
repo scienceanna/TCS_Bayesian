d9cc2546638942bb82579759d95f1e06f927720b
Alasdair <alasdair.clarke@gmail.com>
2020-11-26 12:11:06 +0000

    paper edits

---
+++ analysis/create_plots_for_paper.R
@@ -0,0 +1,60 @@
+library(tidyverse)
+library(brms)
+library(tidybayes)
+library(patchwork)
+library(magrittr)
+
+# set ggplot2 theme
+theme_set(see::theme_lucid())
+
+# use parallel cores for mcmc chains!
+options(mc.cores = parallel::detectCores())
+
+# functions used for the analysis reimplementation
+source("scripts/reimplementation.R")
+
+# functions used for our Bayesian re-analysis
+source("scripts/our_functions.R")
+
+source("scripts/import_and_tidy.R")
+
+
+## Computational Replication of Buetti et al (2019)
+De <- map_dfr(unique(d$exp_id), calc_D_per_feature)
+Dp <- map_df(c(2,4), gen_exp_predictions, De)
+
+## Create summary of results for paper
+left_join(Dp, De, by = c("exp_id", "d_feature")) %>%
+  pivot_longer(
+    cols = c(`best feature`, `orthog. contrast`, collinear),
+    values_to = "Dp",
+    names_to = "method") %>%
+  mutate(method = fct_relevel(method, "best feature", "orthog. contrast")) %>%
+  filter(method == "collinear") %>%
+  ggplot(aes(x = Dp, y = D)) +
+  geom_point( color = "darkblue", alpha = 0.5) + 
+  geom_abline(linetype = 2) +
+  geom_smooth(method = "lm", formula = y ~ x, colour = "violetred3") + 
+  scale_x_continuous("predicted D") + 
+  scale_y_continuous("empirical D")  + 
+  coord_fixed()-> plt_D
+
+exps2predict = c(2, 4)
+
+rt_pred <- map_dfr(exps2predict, predict_rt)
+
+d %>% filter(exp_id %in% exps2predict) %>%
+  group_by(exp_id, p_id, d_feature, N_T) %>%
+  summarise(mean_rt = mean(rt), .groups = "drop") %>%
+  group_by(exp_id,  d_feature, N_T) %>%
+  summarise(mean_rt = mean(mean_rt), .groups = "drop") %>%
+  left_join(rt_pred, by = c("exp_id", "d_feature", "N_T")) %>% 
+  ggplot(aes(x = p_rt, y = mean_rt)) + 
+  geom_point(color = "darkblue", alpha = 0.5) + 
+  geom_abline(linetype = 2) +
+  geom_smooth(method = "lm", formula = y ~ x, colour = "violetred3") + 
+  scale_x_continuous("predicted reaction time (ms)") +
+  scale_y_continuous("empirical mean reaction time (ms)") + 
+  coord_fixed() -> plt_rt
+
+ggsave("../plots/computational_replication.pdf", plt_D + plt_rt, width = 7, height = 3.5)
