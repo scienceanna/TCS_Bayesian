e24971e4c454f21ca545523f4bf085ce52967ac6
Alasdair <alasdair.clarke@gmail.com>
2020-12-04 12:52:15 +0000

    Update problem_with_D.R

--- analysis/problem_with_D.R
+++ analysis/problem_with_D.R
@@ -24,8 +24,6 @@ source("scripts/our_functions.R")
 source("scripts/import_and_tidy.R")
 
 
-
-d$rt <- d$rt * 1000
 ###################################################
 ## Computational Replication of Buetti et al (2019)
 ###################################################
@@ -34,41 +32,47 @@ De <- map_dfr(unique(d$exp_id), calc_D_per_feature)
 
 # what is a typical value of D in exp 1? 
 
-
-
 D2 <- seq(min(filter(De, exp_id == 1)$D),
            max(filter(De, exp_id == 1)$D), length.out = 100)
 
 
-D1 <- min(filter(De, exp_id == 1)$D)
-Dmin <- tibble(
-  Dc = "min",
-  D2 = D2,
-  collinear = 1/((1/D1) + (1/D2)),
-  best_feature = pmin(D1, D2),
-  orth_contrast =  sqrt(1/((1/D1^2) + (1/D2^2)))) %>% 
-  pivot_longer(-c(D2, Dc) ,names_to = "method", values_to = "Dp")
+cs <- c("orange", "yellow", "blue")
+ss <- c("circle", "diamond", "triangle")
 
-D1 <- median(filter(De, exp_id == 1)$D)
-Dmedian <- tibble(
-  Dc = "median",
-  D2 = D2,
-  collinear = 1/((1/D1) + (1/D2)),
-  best_feature = pmin(D1, D2),
-  orth_contrast =  sqrt(1/((1/D1^2) + (1/D2^2)))) %>% 
-  pivot_longer(-c(D2, Dc), names_to = "method", values_to = "Dp")
+df <- tibble()
 
-D1 <- max(filter(De, exp_id == 1)$D)
-Dmax <- tibble(
-  Dc = "max",
-  D2 = D2,
-  collinear = 1/((1/D1) + (1/D2)),
-  best_feature = pmin(D1, D2),
-  orth_contrast =  sqrt(1/((1/D1^2) + (1/D2^2)))) %>% 
-  pivot_longer(-c(D2, Dc), names_to = "method", values_to = "Dp")
+for (col in cs){
+  
+  Dc = filter(De, d_feature == col, exp_id == 1)$D
 
-D <- bind_rows(Dmin, Dmedian, Dmax)
+  Dss = c(
+    filter(De, d_feature == ss[1], exp_id == 1)$D,
+    filter(De, d_feature == ss[2], exp_id == 1)$D,
+    filter(De, d_feature == ss[3], exp_id == 1)$D)
+    
+  df <- bind_rows(df, tibble(
+    colour = col,
+    shape = ss,
+    collinear = 1/((1/Dc) + (1/Dss)),
+    best_feature = pmin(Dc, Dss),
+    orth_contrast =  sqrt(1/((1/Dc^2) + (1/Dss^2)))) %>% 
+    mutate(Dc = Dc, Ds = Dss) %>%
+    pivot_longer(-c(colour, shape, Ds, Dc) ,names_to = "method", values_to = "Dp") )
+   
+  }
+  
 
-ggplot(D, aes(x  = D2, y = Dp, colour = method)) + 
-  geom_point() + facet_wrap(~ Dc)
+# get empirical values for Dcs
+
+d2 <- filter(De, exp_id == 2) %>% separate(d_feature, into = c("colour", "shape")) %>%
+  rename(Dcs = "D") %>%
+  select(-exp_id) %>% full_join(df) %>%
+  mutate(
+    colour = as_factor(colour),
+    colour = fct_relevel(colour, "orange", "yellow", "blue"))
+
+ggplot(d2, aes(x  = Ds, y = Dp, colour = method)) + 
+  geom_line() + geom_point() + 
+  geom_point(aes(y = Dcs), shape = 3, size = 3, colour = "black") +facet_wrap(~ colour) + 
+  scale_x_continuous(breaks = unique(d2$Ds), label = levels(d2$colour))
 
