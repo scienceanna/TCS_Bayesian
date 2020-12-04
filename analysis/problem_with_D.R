af5ae36bd5ac4f7b96fe59892599bf24440e5571
Alasdair <alasdair.clarke@gmail.com>
2020-12-04 14:31:55 +0000

    more D stuff

--- analysis/problem_with_D.R
+++ analysis/problem_with_D.R
@@ -1,7 +1,6 @@
 # the problem with calculating D!
 
 
-
 library(tidyverse)
 library(brms)
 library(tidybayes)
@@ -23,6 +22,43 @@ source("scripts/our_functions.R")
 
 source("scripts/import_and_tidy.R")
 
+calc_D_plot <- function(e_id) {
+  
+  df <- tibble()
+  
+  for (col in cs){
+    
+    Dc = filter(De, d_feature == col, exp_id == 1)$D
+    
+    Dss = c(
+      filter(De, d_feature == ss[1], exp_id == e_id)$D,
+      filter(De, d_feature == ss[2], exp_id == e_id)$D,
+      filter(De, d_feature == ss[3], exp_id == e_id)$D)
+    
+    df <- bind_rows(df, tibble(
+      colour = col,
+      shape = ss,
+      collinear = 1/((1/Dc) + (1/Dss)),
+      best_feature = pmin(Dc, Dss),
+      orth_contrast =  sqrt(1/((1/Dc^2) + (1/Dss^2)))) %>% 
+        mutate(Dc = Dc, Ds = Dss) %>%
+        pivot_longer(-c(colour, shape, Ds, Dc) ,names_to = "method", values_to = "Dp") )
+    
+  }
+  
+  # get empirical values for Dcs
+  
+  d2 <- filter(De, exp_id == e_id+1) %>% separate(d_feature, into = c("colour", "shape")) %>%
+    rename(Dcs = "D") %>%
+    select(-exp_id) %>% full_join(df)
+  
+  ggplot(d2, aes(x  = Ds, y = Dp, colour = method)) + 
+    geom_line() + geom_point() + 
+    geom_point(aes(y = Dcs), shape = 3, size = 3, colour = "black") + facet_wrap(~ colour) + 
+    scale_x_continuous(breaks = unique(d2$Ds), label = ss) -> plt
+  
+  return(plt)
+}
 
 ###################################################
 ## Computational Replication of Buetti et al (2019)
@@ -30,49 +66,20 @@ source("scripts/import_and_tidy.R")
 De <- map_dfr(unique(d$exp_id), calc_D_per_feature)
 
 
-# what is a typical value of D in exp 1? 
-
-D2 <- seq(min(filter(De, exp_id == 1)$D),
-           max(filter(De, exp_id == 1)$D), length.out = 100)
-
-
+# experiment 1 features
 cs <- c("orange", "yellow", "blue")
 ss <- c("circle", "diamond", "triangle")
 
-df <- tibble()
+plt1 <- calc_D_plot(1)
 
-for (col in cs){
-  
-  Dc = filter(De, d_feature == col, exp_id == 1)$D
 
-  Dss = c(
-    filter(De, d_feature == ss[1], exp_id == 1)$D,
-    filter(De, d_feature == ss[2], exp_id == 1)$D,
-    filter(De, d_feature == ss[3], exp_id == 1)$D)
-    
-  df <- bind_rows(df, tibble(
-    colour = col,
-    shape = ss,
-    collinear = 1/((1/Dc) + (1/Dss)),
-    best_feature = pmin(Dc, Dss),
-    orth_contrast =  sqrt(1/((1/Dc^2) + (1/Dss^2)))) %>% 
-    mutate(Dc = Dc, Ds = Dss) %>%
-    pivot_longer(-c(colour, shape, Ds, Dc) ,names_to = "method", values_to = "Dp") )
-   
-  }
-  
+# experiment 2 features
+cs <- c("yellow", "orange", "blue")
+ss <- c("circle", "diamond", "semicircle")
 
-# get empirical values for Dcs
+plt3 <- calc_D_plot(3)
 
-d2 <- filter(De, exp_id == 2) %>% separate(d_feature, into = c("colour", "shape")) %>%
-  rename(Dcs = "D") %>%
-  select(-exp_id) %>% full_join(df) %>%
-  mutate(
-    colour = as_factor(colour),
-    colour = fct_relevel(colour, "orange", "yellow", "blue"))
+plt1 / plt3
 
-ggplot(d2, aes(x  = Ds, y = Dp, colour = method)) + 
-  geom_line() + geom_point() + 
-  geom_point(aes(y = Dcs), shape = 3, size = 3, colour = "black") +facet_wrap(~ colour) + 
-  scale_x_continuous(breaks = unique(d2$Ds), label = levels(d2$colour))
 
+ggsave("../plots/D_sensitivity.pdf", width = 8, height = 4)
