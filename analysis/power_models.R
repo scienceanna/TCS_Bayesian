8e20057e195212c5452878b9307623cb4df63155
scienceanna <hughesannae@gmail.com>
2020-12-14 16:16:14 +0000

    power things

--- analysis/power_models.R
+++ analysis/power_models.R
@@ -27,10 +27,11 @@ my_f <- bf(rt ~ 1 + log(N_T+1) + (1|p_id),
 get_hdpi_for_subsample <- function(n_trials) {
   
   d %>% group_by(p_id,  N_T) %>%
-    slice_sample(n = 6) -> d_ss
+    slice_sample(n = n_trials) -> d_ss
 
   model_params <- set_up_model(1, "shifted_lognormal")
   model_params$my_formula <- my_f
+  model_params$df <- d_ss
   m <- run_model(model_params, ppc = "no")
   h <- hdci(posterior_samples(m, "logN_TP1"), 0.97)
   w <- h[2] - h[1]
@@ -38,4 +39,13 @@ get_hdpi_for_subsample <- function(n_trials) {
   
 }
 
-w <- map_dbl(c(6, 8, 12, 16, 40), get_hdpi_for_subsample)
+samples <- c(2, 4, 6, 8, 10, 12, 14, 16, 20, 24, 32, 40)
+
+w <- map_dbl(samples, get_hdpi_for_subsample)
+
+
+plt_data <- tibble(
+  samples = samples,
+  w = w)
+
+ggplot(plt_data, aes(samples, w)) + geom_point() + geom_line() + theme_bw()
