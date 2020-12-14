987b7c492337f63f851a1550be304edbabc584f1
Alasdair Clarke <alasdair.clarke@gmail.com>
2020-12-14 16:43:45 +0000

    Update power_models.R

--- analysis/power_models.R
+++ analysis/power_models.R
@@ -24,14 +24,28 @@ my_f <- bf(rt ~ 1 + log(N_T+1) + (1|p_id),
            ndt ~ 1 + (1|p_id))
 
 
-get_hdpi_for_subsample <- function(n_trials) {
-  
-  d %>% group_by(p_id,  N_T) %>%
-    slice_sample(n = n_trials) -> d_ss
 
+orig_model <- readRDS("models/exp_1_sft.models")
+
+n <- list(peeps = 100, trials = 6)
+
+
+
+
+
+get_hdpi_for_subsample <- function(n) {
+  
+  
+  # simulate dataset of required size from original model
+  d %>% modelr::data_grid(N_T, d_feature, p_id = 1:samples$peeps[n])  %>%
+    add_predicted_draws(orig_model, re_formula = NULL, allow_new_levels = TRUE, n = samples$trials[n]) %>%
+    ungroup() %>% 
+    select(p_id, d_feature, N_T, trial = ".draw", rt = ".prediction") -> d_sim
+  
+  # fit new model to simualated data!
   model_params <- set_up_model(1, "shifted_lognormal")
   model_params$my_formula <- my_f
-  model_params$df <- d_ss
+  model_params$df <- d_sim
   m <- run_model(model_params, ppc = "no")
   h <- hdci(posterior_samples(m, "logN_TP1"), 0.97)
   w <- h[2] - h[1]
@@ -39,9 +53,11 @@ get_hdpi_for_subsample <- function(n_trials) {
   
 }
 
-samples <- c(2, 4, 6, 8, 10, 12, 14, 16, 20, 24, 32, 40)
+samples <- modelr::data_grid(d, 
+                             trials = c(6, 12, 40), 
+                             peeps= c(20, 40, 80))
 
-w <- map_dbl(samples, get_hdpi_for_subsample)
+w <- map_dbl(1:nrow(samples), get_hdpi_for_subsample)
 
 
 plt_data <- tibble(
