70e4977cb275fd2c53e32f8b3decb39e1e9b0da9
scienceanna <hughesannae@gmail.com>
2022-05-25 16:19:42 +0100

    Update ring_model.R

--- analysis_revised/1_comp_ver/ring_model.R
+++ analysis_revised/1_comp_ver/ring_model.R
@@ -2,65 +2,42 @@ source("../scripts/import_and_tidy.R")
 summary(d)
 options(mc.cores = 4)
 
-d3 <- filter(d, str_detect(exp_id, "3")) %>%
-  mutate(rt = rt/1000)
-
-d3 %>% group_by(p_id, N_T, d_feature, exp_id, ring) %>%
-  summarise(median_rt = median(rt)) %>%
-  ggplot(aes(x = N_T, y = median_rt, colour = ring)) + geom_jitter() +
-  geom_smooth() + 
-  facet_grid( ~ d_feature)
+d3 <- filter(d, str_detect(exp_id, "3a")) %>%
+  group_by(exp_id, p_id, d_feature, N_T, ring) %>%
+  summarise(mean_rt = mean(rt), .groups = "drop") %>%
+  mutate(d_feature = fct_drop(d_feature))
   
-
 ## Ring model
 
 library(brms)
 
+intercepts = paste("d_feature", str_replace(unique(d3$d_feature), " ", ""), sep = "")
+
 my_prior <- c(
-  #prior_string("normal(-0.5, 0.3)",  class = "b", coef = intercepts),
-  prior_string("normal(0, 0.2)", class = "b"),
-  prior_string("normal(-1, 0.5)", class = "Intercept", dpar = "ndt" ),
-  prior_string("cauchy(0, 0.4)", class = "sigma"),
-  prior_string("cauchy(0, 0.05)", class = "sd"),
-  prior_string("cauchy(0, 0.05)", class = "sd", dpar = "ndt"))
+  prior_string("normal(0, 50)", class = "b"),
+  prior_string("normal(500, 100)", class = "b", coef = intercepts),
+  prior_string("normal(0, 50)", class = "sigma"))
 
-
-my_f <- bf(rt ~ 0 + exp_id:ring + d_feature:ring:log(N_T+1) + (1|p_id), 
-           ndt ~ 1 + (1|p_id))
-
-my_inits <- list(list(Intercept_ndt = -10), list(Intercept_ndt = -10), list(Intercept_ndt = -10), list(Intercept_ndt = -10))
-
-
-m <- brm(formula = my_f,
-         data = d3,
-         family = "shifted_lognormal",
+# ring model
+m <- brm(mean_rt ~  0 + d_feature + log(N_T+1):d_feature:ring, 
+         data = d3, 
          prior = my_prior,
-         init = my_inits,
-         save_pars = save_pars(all = TRUE))
+         iter = 500, 
+         chains = 1,
+         refresh = 0)
 
-saveRDS(m,'ring_model.rds')
-
-## Model without a ring
-
-my_f2 <- bf(rt ~ 0 + exp_id + d_feature:log(N_T+1) + (1|p_id), 
-           ndt ~ 1 + (1|p_id))
-
-m2 <- brm(formula = my_f2,
-         data = d3,
-         family = "shifted_lognormal",
+# no ring model
+m2 <- brm(mean_rt ~  0 + d_feature + log(N_T+1):d_feature, 
+         data = d3, 
          prior = my_prior,
-         init = my_inits, 
-         save_pars = save_pars(all= TRUE))
+         iter = 500, 
+         chains = 1,
+         refresh = 0)
 
-saveRDS(m2, 'nonring_model.rds')
 
-
-####
-
-m <- readRDS('ring_model.rds')
-m2 <- readRDS('nonring_model.rds')
-
-m <- bridge_sampler(m, silent = TRUE)
-m2 <- bridge_sampler(m2, silent = TRUE)
+m <- bridge_sampler(m)
+m2 <- bridge_sampler(m2)
 
 post_prob(m, m2)
+
+
