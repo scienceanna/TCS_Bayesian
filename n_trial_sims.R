b035be68669030e94f20b0bb41df20731ba3fdd0
Alasdair Clarke <alasdair.clarke@gmail.com>
2022-09-27 10:00:00 +0100

    Update n_trial_sims.R

--- n_trial_sims.R
+++ n_trial_sims.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 # sampling from a log normal estiamte acc sim
 
-source("analysis_revised/scripts/import_and_tidy.R")
+#source("analysis_revised/scripts/import_and_tidy.R")
 summary(d)
 
 mu <- 6.315
@@ -13,16 +13,15 @@ do_calc <- function(n, mu, sigma) {
   
   return(tibble(n = n,
                 mean_rt = mean(samples),
-                mean_log_rt = mean(log(samples)),
-                median_rt = median(samples)))
+                mean_log_rt = mean(log(samples))))
   
 }
 
 
-d <- map_dfr(rep(seq(6,40, by = 2), each = 1000), do_calc, mu, sigma)
+d <- map_dfr(rep(c(10, 12, 15, 20, 30, 40), each = 10000), do_calc, mu, sigma)
 
- gt <- tibble(summary = c("mean_log_rt", "mean_rt", "median_rt"),
-              ground_truth = c(mu, exp(mu + (sigma^2)/2), 526))
+gt <- tibble(summary = c("mean_log_rt", "mean_rt"),
+              ground_truth = c(mu, exp(mu + (sigma^2)/2)))
 
 
 d %>% pivot_longer(-n, names_to = "summary", values_to = "stat") %>%
@@ -36,8 +35,22 @@ d %>%
   geom_hline(data = gt, aes(yintercept = ground_truth), linetype = 2)
   
   
-d %>% mutate(abs_err = abs(stat - ground_truth)) %>%
-  ggplot(aes(n, abs_err)) + 
-  tidybayes::stat_lineribbon(alpha = 0.5) +
-  facet_wrap(~summary)
+d %>% mutate(err = (stat - ground_truth)) %>%
+  group_by(n, summary) %>%
+  summarise(err0.05 = quantile(err, 0.05),
+            err0.25 = quantile(err, 0.25),
+            err0.75 = quantile(err, 0.75),
+            err0.95 = quantile(err, 0.95)) %>%
+  filter(summary == "mean_rt") %>%
+  select(-summary) -> derr
+
+derr$err0.05 <- derr$err0.05/max(derr$err0.05)
+derr$err0.25 <- derr$err0.25/max(derr$err0.25)
+derr$err0.75 <- derr$err0.75/min(derr$err0.75)
+derr$err0.95 <- derr$err0.95/min(derr$err0.95)
+
+
+derr %>%
+  knitr::kable(digits = 2)
+ 
   
